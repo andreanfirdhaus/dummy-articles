a43292f987c46175d1edaf75db5d86a5b26793a7
andreanfirdhaus <andreanfirdhaus22@gmail.com>
2024-10-17 22:50:11 +0700

    style: line formatter

--- src/api/loader.js
+++ src/api/loader.js
@@ -1,7 +1,12 @@
 export const posts = () => {
-    return fetch(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${import.meta.env.VITE_SECRET_KEY}`);
-}
+    return fetch(
+        `https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${import.meta.env.VITE_SECRET_KEY
+        }`
+    );
+};
 
 export const github = () => {
-    return fetch(`https://api.github.com/users/${import.meta.env.VITE_USER}/repos`)
-}
+    return fetch(
+        `https://api.github.com/users/${import.meta.env.VITE_USER}/repos`
+    );
+};
